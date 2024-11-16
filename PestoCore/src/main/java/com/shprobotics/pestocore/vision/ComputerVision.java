3e7400843cbd7534136113076ee84879d6ffdd5d
William Speiser <williamspeiser@icloud.com>
2024-11-15 22:29:08 -0800

    Update ComputerVision.java

--- PestoCore/src/main/java/com/shprobotics/pestocore/vision/ComputerVision.java
+++ PestoCore/src/main/java/com/shprobotics/pestocore/vision/ComputerVision.java
@@ -1,15 +1,25 @@
 package com.shprobotics.pestocore.vision;
 
+import com.shprobotics.pestocore.geometries.Pose2D;
+
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ComputerVision {
+    public static Mat convertColor(Mat input, int code) {
+        Mat out = new Mat();
+        Imgproc.cvtColor(input, out, code);
+        return out;
+    }
+
     public static Mat filterColor(Mat input, Scalar low, Scalar high) {
         Mat out = new Mat();
         Core.inRange(input, low, high, out);
@@ -30,5 +40,38 @@ public class ComputerVision {
         Imgproc.findContours(input, contours, hierarchy, mode, method);
         hierarchy.release();
         return contours;
+    }
+
+    public static Mat blur(Mat input, Size size) {
+        Mat output = new Mat();
+        Imgproc.blur(input, output, size);
+        return output;
+    }
+
+    public static Pose2D getPose(Mat input) {
+        Moments moments = Imgproc.moments(input);
+
+        double x = moments.get_m10() / moments.get_m00();
+        double y = moments.get_m01() / moments.get_m00();
+
+        double a = moments.get_m20() / moments.get_m00() - x * x;
+        double b = 2 * (moments.get_m11() / moments.get_m00() - x * y);
+        double c = moments.get_m02() / moments.get_m00() - y * y;
+
+        double theta = 0.5 * Math.atan(b / (a - c)) + (a < c ? Math.PI / 2 : 0);
+
+//        % Barycenter
+//        E.x = E.m10/E.m00;
+//        E.y = E.m01/E.m00;
+//
+//        % Central moments (intermediary step)
+//        a = E.m20/E.m00 - E.x^2;
+//        b = 2*(E.m11/E.m00 - E.x*E.y);
+//        c = E.m02/E.m00 - E.y^2;
+//
+//        % Orientation (radians)
+//        E.theta = 1/2*atan(b/(a-c)) + (a<c)*pi/2;
+
+        return new Pose2D(x, y, theta);
     }
 }
