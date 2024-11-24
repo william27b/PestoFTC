48acca76eb0c7cda5ea227e9dc08bf90a4b2189f
William Speiser <williamspeiser@icloud.com>
2024-11-23 22:40:44 -0800

    Update ParametricHeading.java

--- PestoCore/src/main/java/com/shprobotics/pestocore/geometries/ParametricHeading.java
+++ PestoCore/src/main/java/com/shprobotics/pestocore/geometries/ParametricHeading.java
@@ -1,12 +1,26 @@
 package com.shprobotics.pestocore.geometries;
 
+import org.apache.commons.math3.util.MathUtils;
+
 public class ParametricHeading {
     private final double startHeading;
     private final double endHeading;
 
     public ParametricHeading(double startHeading, double endHeading) {
         this.startHeading = startHeading;
-        this.endHeading = endHeading;
+        this.endHeading = MathUtils.normalizeAngle(endHeading, startHeading);
+    }
+
+    public ParametricHeading(double startHeading, double endHeading, boolean CW) {
+        this.startHeading = startHeading;
+        endHeading = MathUtils.normalizeAngle(endHeading, startHeading);
+
+        if (CW)
+            if (endHeading > this.startHeading) this.endHeading = endHeading - (2*Math.PI);
+            else this.endHeading = endHeading;
+        else
+            if (endHeading < this.startHeading) this.endHeading = endHeading + (2*Math.PI);
+            else this.endHeading = endHeading;
     }
 
     public double getHeading(double t) {
