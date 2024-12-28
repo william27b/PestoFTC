3c5f177cd3fa770961826363d643a54eb121bb9f
William Speiser <williamspeiser@icloud.com>
2024-12-27 22:32:38 -0800

    get rid of monte carlo

--- PestoCore/src/main/java/com/shprobotics/pestocore/sensors/SensorData.java
+++
@@ -1,8 +0,0 @@
-package com.shprobotics.pestocore.sensors;
-
-import com.shprobotics.pestocore.geometries.Pose2D;
-
-public interface SensorData {
-    void update();
-    double getProbability(Pose2D pose2D);
-}
