0bd9e7b81974d968a3f427f91f0d1e2e1ffa3c81
William Speiser <williamspeiser@icloud.com>
2024-12-31 14:34:53 -0800

    Create LocalizatonTuner.java

---
+++ PestoCore/src/main/java/com/shprobotics/pestocore/tuners/LocalizatonTuner.java
@@ -0,0 +1,48 @@
+package com.shprobotics.pestocore.tuners;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.shprobotics.pestocore.drivebases.DeterministicTracker;
+import com.shprobotics.pestocore.drivebases.MecanumController;
+import com.shprobotics.pestocore.drivebases.TeleOpController;
+import com.shprobotics.pestocore.geometries.Pose2D;
+import com.shprobotics.pestocore.geometries.Vector2D;
+
+public abstract class LocalizatonTuner extends LinearOpMode {
+    MecanumController mecanumController;
+    DeterministicTracker tracker;
+    TeleOpController teleOpController;
+
+    public abstract void setMecanumController(HardwareMap hardwareMap);
+    public abstract void setTracker(HardwareMap hardwareMap);
+    public abstract void setTeleOpController(MecanumController mecanumController, DeterministicTracker tracker, HardwareMap hardwareMap);
+
+    @Override
+    public void runOpMode() {
+        setMecanumController(hardwareMap);
+        setTracker(hardwareMap);
+        setTeleOpController(mecanumController, tracker, hardwareMap);
+
+        waitForStart();
+
+        while (opModeIsActive() && !isStopRequested()) {
+            teleOpController.driveRobotCentric(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+            tracker.update();
+
+            Vector2D currentPosition = tracker.getCurrentPosition().asVector();
+            Pose2D currentVelocity = tracker.getRobotVelocity();
+            telemetry.addData("X", currentPosition.getX());
+            telemetry.addData("Y", currentPosition.getY());
+            telemetry.addData("Rotation", tracker.getCurrentPosition().getHeadingRadians());
+            telemetry.addLine();
+            telemetry.addData("X velocity", currentVelocity.getX());
+            telemetry.addData("Y velocity", currentVelocity.getY());
+            telemetry.addData("Rotational velocity", currentVelocity.getHeadingRadians());
+            telemetry.update();
+
+            if (gamepad1.b) {
+                tracker.reset();
+            }
+        }
+    }
+}
