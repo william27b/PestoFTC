f91a4d410c03be6c55dea6e01df5ac1361fcd8e2
William Speiser <williamspeiser@icloud.com>
2025-01-07 16:51:15 -0800

    heading

--- PestoCore/src/main/java/com/shprobotics/pestocore/geometries/ParametricHeading.java
+++ PestoCore/src/main/java/com/shprobotics/pestocore/geometries/ParametricHeading.java
@@ -1,30 +1,57 @@
 package com.shprobotics.pestocore.geometries;
 
-import org.apache.commons.math3.util.MathUtils;
-
 public class ParametricHeading {
     private final double[] headings;
     private final int n;
+    private double t;
 
     public ParametricHeading(double[] headings) {
         assert headings.length >= 2;
 
         this.headings = headings;
         this.n = headings.length;
+        this.t = 0;
+    }
+
+    public void reset() { this.t = 0; }
+
+    public void increment(double dt) { this.t = Math.min(1, Math.max(0, this.t + dt)); }
+
+    public double getT() {
+        return this.t;
     }
 
     public double getHeading(double t) {
-        assert t >= 0 && t <= 1;
-
-        if (t == 1)
+        if (t == 0) {
+            return headings[0];
+        }
+        if (t == 1) {
             return headings[n-1];
+        }
 
-        int startIndex = (int) ((headings.length - 1) * t);
-        double startHeading = headings[startIndex];
-        double endHeading = headings[startIndex+1];
+        double point = 0.0;
 
-        t *= (startIndex + 1.0) / n;
+        double n_factorial = 1;
+        for (int i = 1; i < n; i++) {
+            n_factorial *= i;
+        }
+        double i_factorial = 1;
+        double n_minus_i_factorial = n_factorial * n;
 
-        return MathUtils.normalizeAngle(startHeading + (endHeading - startHeading) * t, 0.0);
+        for (int i = 0; i < n; i++) {
+            n_minus_i_factorial /= (n - i);
+
+            double copy = headings[i];
+            copy *= ((n_factorial / (i_factorial * n_minus_i_factorial)) * Math.pow(1 - t, n - i - 1) * Math.pow(t, i));
+
+            i_factorial *= i + 1;
+            point += copy;
+        }
+
+        return point;
+    }
+
+    public double getHeading() {
+        return getHeading(this.t);
     }
 }
--- PestoCore/src/main/java/com/shprobotics/pestocore/geometries/PathContainer.java
+++ PestoCore/src/main/java/com/shprobotics/pestocore/geometries/PathContainer.java
@@ -1,10 +1,14 @@
 package com.shprobotics.pestocore.geometries;
 
+import static java.lang.Math.abs;
+
 import java.util.ArrayList;
 
 public class PathContainer {
     private final ArrayList<BezierCurve> curves;
     private final ArrayList<ParametricHeading> headings;
+    private final Vector2D startPosition;
+    private Vector2D currentPosition;
     private double heading;
     private final ArrayList<Runnable> actions;
     private boolean executed;
@@ -16,6 +20,8 @@ public class PathContainer {
     public PathContainer(PathContainerBuilder pathContainerBuilder) {
         this.curves = pathContainerBuilder.curves;
         this.headings = pathContainerBuilder.headings;
+        this.startPosition = pathContainerBuilder.startPosition;
+        this.currentPosition = this.startPosition.copy();
         this.heading = 0;
         this.actions = pathContainerBuilder.actions;
         this.executed = false;
@@ -26,11 +32,12 @@ public class PathContainer {
     }
 
     public void reset() {
-        this.i = 0;
         for (BezierCurve curve: curves) {
             curve.reset();
         }
+        this.currentPosition = this.startPosition.copy();
         executed = false;
+        this.i = 0;
     }
 
     public Vector2D getEndpoint() {
@@ -45,8 +52,41 @@ public class PathContainer {
         return executed;
     }
 
-    public Vector2D getNextPosition(Vector2D robotPosition) {
+    public Vector2D getNextPosition(Pose2D robotPosition) {
+        Vector2D robotVector = robotPosition.asVector();
         BezierCurve current = curves.get(i);
+
+        if (current == null) {
+            ParametricHeading currentHeading = headings.get(i);
+            if (currentHeading.getT() == 1) {
+                if (this.actions.get(i) != null) {
+                    this.actions.get(i).run();
+                    if (i == n-1)
+                        executed = true;
+                }
+                this.i = Math.min(n-1, i+1);
+                return getNextPosition(robotPosition);
+            }
+
+            currentHeading.increment(increment);
+
+            if (heading == currentHeading.getHeading()) {
+                currentHeading.increment(increment);
+            }
+
+            double nextHeading = currentHeading.getHeading();
+
+            // TODO: check math
+            while (abs(nextHeading - robotPosition.getHeadingRadians()) < abs(heading - robotPosition.getHeadingRadians())) {
+                heading = nextHeading;
+                currentHeading.increment(increment);
+                nextHeading = currentHeading.getHeading();
+            }
+
+            this.heading = headings.get(i).getHeading();
+            return this.currentPosition;
+        }
+
         if (current.getT() == 1) {
             if (this.actions.get(i) != null) {
                 this.actions.get(i).run();
@@ -60,13 +100,13 @@ public class PathContainer {
         Vector2D currentPosition = current.getPoint();
         current.increment(increment);
 
-        if (Vector2D.equals(currentPosition, robotPosition)) {
+        if (Vector2D.equals(currentPosition, robotVector)) {
             current.increment(increment);
         }
 
         Vector2D nextPosition = current.getPoint();
 
-        while (Vector2D.fastdist(nextPosition, robotPosition) < Vector2D.fastdist(currentPosition, robotPosition)) {
+        while (Vector2D.fastdist(nextPosition, robotVector) < Vector2D.fastdist(currentPosition, robotVector)) {
             currentPosition = nextPosition;
             current.increment(increment);
             nextPosition = current.getPoint();
@@ -74,6 +114,7 @@ public class PathContainer {
 
         this.heading = headings.get(i) != null ? headings.get(i).getHeading(current.getT()) : this.heading;
 
+        this.currentPosition = currentPosition;
         return currentPosition;
     }
 
@@ -85,6 +126,7 @@ public class PathContainer {
         private final ArrayList<BezierCurve> curves;
         private final ArrayList<ParametricHeading> headings;
         private final ArrayList<Runnable> actions;
+        private Vector2D startPosition;
 
         private double increment;
 
@@ -92,8 +134,14 @@ public class PathContainer {
             this.curves = new ArrayList<>();
             this.headings = new ArrayList<>();
             this.actions = new ArrayList<>();
+            this.startPosition = new Vector2D(0, 0);
 
             this.increment = 0.0005;
+        }
+
+        public PathContainerBuilder setStartPosition(Vector2D startPosition) {
+            this.startPosition = startPosition;
+            return this;
         }
 
         public PathContainerBuilder setIncrement(double increment) {
@@ -104,6 +152,13 @@ public class PathContainer {
         public PathContainerBuilder addCurve(BezierCurve curve) {
             this.curves.add(curve);
             this.headings.add(null);
+            this.actions.add(null);
+            return this;
+        }
+
+        public PathContainerBuilder addCurve(ParametricHeading heading) {
+            this.curves.add(null);
+            this.headings.add(heading);
             this.actions.add(null);
             return this;
         }
